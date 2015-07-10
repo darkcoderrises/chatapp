658e540e36405d7802dc245317aa34153ceca703
harshil goel <harshil158@gmail.com>
2015-07-10 17:56:48 +0530

    objected my lord

--- index.js
+++ index.js
@@ -3,17 +3,21 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-var nicks = new Array(1000);
+function Person(name,addr)
+{
+    this.name=name;
+    this.addr=addr;
+}
 
+var LIMIT=3;
 
-for(var i=0;i<1000;i++){
-    nicks[i] = new Array(2);
-    nicks[i][0]=0;
+var nicks = new Array(LIMIT);
+
+for(var i=0;i<LIMIT;i++){
+    nicks[i]=new Person("",0);
 }
 
 app.use(express.static(__dirname, '/css'));
-
-
 app.get('/', function(req, res){
     res.sendFile(__dirname + '/index.html');
 });
@@ -22,11 +26,11 @@ app.get('/', function(req, res){
 
 function update(){
     io.emit('empty');
-    for(var i=0;i<1000;i++)
+    for(var i=0;i<LIMIT;i++)
     {
-        if(nicks[i][0]!=0)
+        if(nicks[i].addr!=0 & nicks[i].name!="")
         {
-            io.emit('addonline',nicks[i][1]);
+            io.emit('addonline',nicks[i].name);
         }
     }
 }
@@ -34,44 +38,40 @@ function update(){
 
 io.on('connection', function(socket){
     var address=socket.handshake.address;
-    var name=-1,add=-1;
+    var person=-1;
     update();
-    for (var i=0;i<1000;i++)
+    for (var i=0;i<LIMIT;i++)
     {
-    if (nicks[i][0]==address){
-        name=nicks[i][1];
-        add=i;
-    break;
+        if(nicks[i].addr==address)
+        {
+            person=i;
+        }
+        break;
     }
-}
-if(name==-1)
+if(person==-1)
 {
-    for (var i=0;i<1000;i++){   
-        if(nicks[i][0]==0){
-            add=i;
-            nicks[i][0]=0;
-            nicks[i][1]=address;
-            name=nicks[i][0];  
+    for (var i=0;i<LIMIT;i++){   
+        if(nicks[i].addr==0){
+            person=i;
+            nicks[i]=new Person("",address);
             break;
         }
     }
 }
-
-if(name==-1)
+if(person==-1)
     socket.end();
 
 socket.on('chat message', function(msg){
+    console.log(msg);
     if (msg.charAt(0)=='/'){
-        name = msg.substring(1,msg.length);
-        nicks[add][0]=nicks[add][1];
-        nicks[add][1]=name;
+        nicks[person].name = msg.substring(1,msg.length);
         update();
     }
     else{
         var currentdate=new Date();
         var a = (currentdate.getHours() + ":" + currentdate.getMinutes() + ":" + currentdate.getSeconds());
-        if(msg.length<1000 && name!=address){
-            var b = [name+":"+msg,a];
+        if(msg.length<100 && person.name!=""){
+            var b = [nicks[person].name+":"+msg,a];
             console.log(b);
             io.emit('chat message',b);
         }
@@ -81,8 +81,7 @@ socket.on('chat message', function(msg){
 socket.on('disconnect', function () {
     var string = "user disconnected";
     console.log(string);
-    nicks[add][0]=0;
-    nicks[add][1]=0;
+    nicks[i]=new Person("",0);
     update();
 });
 
